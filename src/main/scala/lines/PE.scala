a96f5004efe55265e2d98004529c3ec356554e4d
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2016-10-25 13:23:35 +0200

    add PE line

--- src/main/scala/lines/PE.scala
+++ src/main/scala/lines/PE.scala
@@ -1,3 +1,15 @@
 package bio4j.data.uniprot.lines
 
-case class PE(val lines: Seq[String]) extends AnyVal
+import bio4j.data.uniprot.{ ProteinExistence, EvidenceAtProteinLevel, EvidenceAtTranscriptLevel, InferredFromHomology, Predicted, Uncertain }
+
+case class PE(val line: String) extends AnyVal {
+
+  def proteinExistence: ProteinExistence =
+    (line take 1) match {
+      case "1" => EvidenceAtProteinLevel
+      case "2" => EvidenceAtTranscriptLevel
+      case "3" => InferredFromHomology
+      case "4" => Predicted
+      case "5" => Uncertain
+    }
+}
